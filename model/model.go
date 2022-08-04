0c0944d6476d69367d35aca30447598e6dda707f
nil <n.spiller@gocaspi.de>
2022-08-04 11:40:24 +0200

    changed ts config

--- model/model.go
+++ model/model.go
@@ -1,10 +1,10 @@
 package model
 
 type Employee struct {
-	ID          string `json:"id"`
-	FirstName   string `json:"first_name"`
-	LastName    string `json:"last_name"`
-	Email       string `json:"email"`
+	ID        string `json:"id"`
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+	Email     string `json:"email"`
 }
 
 type Payload struct {
