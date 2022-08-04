461c3946d1a903483782f849a4fd55f9922f8612
nil <n.spiller@gocaspi.de>
2022-08-04 11:59:31 +0200

    feature/ON4T-76_initializeProject_restructuring: - added Interfaces to the api in models for user, timeentry and Proposals

--- model/model.go
+++ model/model.go
@@ -1,5 +1,7 @@
 package model
 
+import "time"
+
 type Employee struct {
 	ID        string `json:"id"`
 	FirstName string `json:"first_name"`
@@ -14,4 +16,33 @@ type Payload struct {
 type DbConfig struct {
 	URL      string
 	Database string
+}
+
+type User struct {
+	Username          string   `json:"username" bson:"username"`
+	Password          string   `json:"password" bson:"password"`
+	FirstName         string   `json:"first_name" bson:"first_name"`
+	LastName          string   `json:"last_name" bson:"lastName"`
+	Email             string   `json:"email" bson:"email"`
+	Team              string   `json:"team" bson:"team"`
+	Projects          []string `json:"projects" bson:"projects"`
+	TotalWorkingHours float32  `json:"totalWorkingHours" bson:"totalWorkingHours"`
+	VacationDays      int      `json:"vacationDays" bson:"vacationDays"`
+}
+
+type TimeEntry struct {
+	UserId     string    `json:"userId" bson:"userId"`
+	Start      time.Time `json:"start" bson:"start"`
+	End        time.Time `json:"end" bson:"end"`
+	BreakStart time.Time `json:"breakStart" bson:"breakStart"`
+	BreakEnd   time.Time `json:"breakEnd" bson:"breakEnd"`
+	Project    string    `json:"project" bson:"project"`
+}
+
+type Proposal struct {
+	UserId    string    `json:"userId" bson:"userId"`
+	StartDate time.Time `json:"startDate" bson:"startDate"`
+	EndDate   time.Time `json:"endDate" bson:"endDate"`
+	Approved  bool      `json:"approved" bson:"approved"`
+	Type      string    `json:"type" bson:"type"`
 }
