cbdd9eafcf81218e25b57d9e96a6e2aca1c96f2c
Luca Wolckenhauer <l.wolckenhauer@gocaspi.de>
2022-08-17 16:28:54 +0200

    fixed TeamMember model

--- model/model.go
+++ model/model.go
@@ -81,8 +81,8 @@ type UserUpdateResult struct {
 type TeamMember struct {
 	ID        primitive.ObjectID `json:"id" bson:"_id"`
 	Username  string             `json:"username" bson:"username"`
-	FirstName string             `json:"first_name" bson:"first_name"`
-	LastName  string             `json:"last_name" bson:"lastName"`
+	FirstName string             `json:"firstname" bson:"firstname"`
+	LastName  string             `json:"lastname" bson:"lastname"`
 	Email     string             `json:"email" bson:"email"`
 }
 
