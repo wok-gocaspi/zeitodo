8f43a2df31d0443776b74efe8335efda058970d3
Luca Wolckenhauer <l.wolckenhauer@gocaspi.de>
2022-08-18 13:23:20 +0200

    Hotfix: Changed workflow config

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"example-project/datasource"
 	"example-project/middleware"
 	"example-project/model"
 	"github.com/gin-gonic/gin"
@@ -21,7 +22,7 @@ func main() {
 	} else {
 		port = "9090"
 	}
-	databaseClient := tasource.NewDbClient(model.DbConfig{
+	databaseClient := datasource.NewDbClient(model.DbConfig{
 
 		//URL:      "mongodb://on4tdb:BMZQuk6pIL39nq46fOQPzygHtrhad5MFQMxs8YBQDW6YsJQSgbsIwO3aeOzlXXEnTjoz7ADVVr9jE1PKzU6GyQ==@on4tdb.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@on4tdb@",
 		URL:      os.Getenv("MONGO_URL"),
